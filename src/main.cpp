a1dd02193de4d86b521c6bfb9c66a6c54aa0f552
namin <namin@iastate.edu>
2025-12-12 08:31:20 -0600

    hittable sampling

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@
 
 #include "scene/camera.h"
 
+
+    // Generic light sources
+    auto empty_material = shared_ptr<material>();
+    quad lights(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), empty_material);
+
 void bouncing_spheres_demo()
 {
     hittable_list world;
@@ -83,7 +88,7 @@ void bouncing_spheres_demo()
     cam.defocus_angle = 0.6;
     cam.focus_dist = 10.0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void checkered_spheres_demo()
@@ -110,7 +115,7 @@ void checkered_spheres_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void earth_demo()
@@ -134,7 +139,7 @@ void earth_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(hittable_list(globe));
+    cam.render(hittable_list(globe), lights);
 }
 
 void perlin_spheres_demo()
@@ -160,7 +165,7 @@ void perlin_spheres_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void quads_demo()
@@ -196,7 +201,7 @@ void quads_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void simple_light_demo()
@@ -226,7 +231,7 @@ void simple_light_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void cornell_box_demo()
@@ -275,7 +280,7 @@ void cornell_box_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void cornell_smoke_demo()
@@ -320,7 +325,7 @@ void cornell_smoke_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void final_scene_demo(int image_width, int samples_per_pixel, int max_depth)
@@ -400,7 +405,7 @@ void final_scene_demo(int image_width, int samples_per_pixel, int max_depth)
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 void file_load_demo()
@@ -428,7 +433,7 @@ void file_load_demo()
 
     cam.defocus_angle = 0;
 
-    cam.render(world);
+    cam.render(world, lights);
 }
 
 /// @brief A function to quickly test all demos to make sure everything still works
--- src/scene/camera.h
+++ src/scene/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "scene-objects/hittable.h"
+#include "../util/general/pdf.h"
 #include "materials/material.h"
 
 #include <thread>
@@ -29,7 +30,7 @@ public:
 
     std::mutex log_mutex;
 
-    void render(const hittable &world)
+    void render(const hittable &world, const hittable &lights)
     {
         initialize();
 
@@ -46,10 +47,12 @@ public:
             {
                 color pixel_color(0, 0, 0);
 
-                for (int s_j = 0; s_j < sqrt_spp; s_j++) {
-                    for (int s_i = 0; s_i < sqrt_spp; s_i++) {
+                for (int s_j = 0; s_j < sqrt_spp; s_j++)
+                {
+                    for (int s_i = 0; s_i < sqrt_spp; s_i++)
+                    {
                         ray r = get_ray(i, j, s_i, s_j);
-                        pixel_color += ray_color(r, max_depth, world);
+                        pixel_color += ray_color(r, max_depth, world, lights);
                     }
                 }
 
@@ -181,8 +184,8 @@ private:
         return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
-    color ray_color(const ray &r, int depth, const hittable &world) const
-    {
+    color ray_color(const ray& r, int depth, const hittable& world, const hittable& lights)
+    const {
         // If we've exceeded the ray bounce limit, no more light is gathered.
         if (depth <= 0)
             return color(0, 0, 0);
@@ -200,26 +203,14 @@ private:
         if (!rec.mat->scatter(r, rec, attenuation, scattered, pdf_value))
             return color_from_emission;
 
-        auto on_light = point3(random_double(213,343), 554, random_double(227,332));
-        auto to_light = on_light - rec.p;
-        auto distance_squared = to_light.length_squared();
-        to_light = unit_vector(to_light);
-
-        if (dot(to_light, rec.normal) < 0)
-            return color_from_emission;
-
-        double light_area = (343-213)*(332-227);
-        auto light_cosine = std::fabs(to_light.y());
-        if (light_cosine < 0.000001)
-            return color_from_emission;
-
-        pdf_value = distance_squared / (light_cosine * light_area);
-        scattered = ray(rec.p, to_light, r.time());
+        hittable_pdf light_pdf(lights, rec.p);
+        scattered = ray(rec.p, light_pdf.generate(), r.time());
+        pdf_value = light_pdf.value(scattered.direction());
 
         double scattering_pdf = rec.mat->scattering_pdf(r, rec, scattered);
 
-        color color_from_scatter =
-            (attenuation * scattering_pdf * ray_color(scattered, depth - 1, world)) / pdf_value;
+        color sample_color = ray_color(scattered, depth-1, world, lights);
+        color color_from_scatter = (attenuation * scattering_pdf * sample_color) / pdf_value;
 
         return color_from_emission + color_from_scatter;
     }
